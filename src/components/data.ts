3655f62e650585352522f0ff02ddc266b763e251
msaouab <msaouab@student.1337.ma>
2023-03-22 22:47:01 +0000

    first test done

---
+++ src/components/data.ts
@@ -0,0 +1,62 @@
+import	github from '../assets/github.svg'
+import	instagram from '../assets/instagram.svg'
+import	linkedin from '../assets/linkedin.svg'
+import	email from '../assets/email.svg'
+import	telegram from '../assets/telegram.svg'
+import	twitter from '../assets/twitter.svg'
+import	whatsapp from '../assets/whatsapp.svg'
+import styled from 'styled-components'
+
+const listSocialmedia = [
+	{
+		id: 0,
+		link: "mailto:saouab.mohammed@gmail.com",
+		name: "Mail",
+		cls: "Mail",
+		icon: email,
+	},
+	{
+		id: 1,
+		link: "https://github.com/msaouab",
+		name: "Github",
+		cls: "Github",
+		icon: github,
+	},
+	{
+		id: 2,
+		link: "https://www.linkedin.com/in/msaouab/",
+		name: "Linkedin",
+		cls: "Linkedin",
+		icon: linkedin,
+	},
+	{
+		id: 3,
+		link: "https://twitter.com/msaouab",
+		name: "Twitter",
+		cls: "Twitter",
+		icon: twitter,
+	},
+	{
+		id: 4,
+		link: "https://www.instagram.com/themedsaouab/",
+		name: "Instagram",
+		cls: "Instagram",
+		icon: instagram,
+	},
+	{
+		id: 5,
+		link: "https://wa.me/qr/EUWI4W2PDJJOF1",
+		name: "Whatsapp",
+		cls: "Whatsapp",
+		icon: whatsapp,
+	},
+	// {
+	// 	id: 6,
+	// 	link: "https://wa.me/qr/EUWI4W2PDJJOF1",
+	// 	name: "Telegram",
+	// 	cls: "Telegram",
+	// 	icon: telegram,
+	// }
+]
+
+export default listSocialmedia
